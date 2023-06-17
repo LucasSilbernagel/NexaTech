2da09893ab6eedde4abdbce317f6f7ea7e40987f
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-06-16 22:47:24 -0400

    navbar update

--- client/src/app/components/Navbar/Navbar.tsx
+++ client/src/app/components/Navbar/Navbar.tsx
@@ -1,33 +1,72 @@
+'use client'
 import Image from 'next/image'
-import { groq } from 'next-sanity'
 import Link from 'next/link'
-import { clientFetch } from '../../clientFetch'
+import { FaShoppingCart } from 'react-icons/fa'
+import customClient from '../../customClient'
+import { useEffect, useState } from 'react'
 
-export default async function Navbar() {
-  const navBarData = await clientFetch(
-    groq`*[_type == 'navbar']{
+interface INavBarData {
+  logoLinkUrl: string
+  logoLinkAriaLabel: string
+  logoLinkImage: string
+}
+
+export default function Navbar() {
+  const [navBarData, setNavBarData] = useState<INavBarData[]>([])
+
+  useEffect(() => {
+    customClient
+      .fetch(
+        `*[_type == 'navbar']{
       "logoLinkUrl": logoLink.url,
       "logoLinkAriaLabel": logoLink.ariaLabel,
       "logoLinkImage": logoLink.image.asset->url
     }`
-  )
-  return (
-    <nav>
-      <Link
-        href={navBarData[0].logoLinkUrl}
-        aria-label={navBarData[0].logoLinkAriaLabel}
-      >
-        <div className="w-[100px]">
-          <Image
-            src={navBarData[0].logoLinkImage}
-            alt=""
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: 'auto' }}
-          />
+      )
+      .then((data) => setNavBarData(data))
+      .catch(console.error)
+  }, [])
+
+  if (navBarData.length > 0) {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link
+              href={navBarData[0].logoLinkUrl}
+              aria-label={navBarData[0].logoLinkAriaLabel}
+            >
+              <div className="w-[150px]">
+                <Image
+                  src={navBarData[0].logoLinkImage}
+                  alt=""
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                />
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+        <div>
+          <button
+            aria-label="shopping cart"
+            onClick={() => alert('Products available for purchase soon!')}
+          >
+            <FaShoppingCart />
+          </button>
         </div>
-      </Link>
-    </nav>
-  )
+      </nav>
+    )
+  } else return null
 }
