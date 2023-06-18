0c07af8ec315105edfa5236e9155d7a1f61bcf50
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-06-18 17:50:31 -0400

    setting up nav bar

--- client/src/app/components/Navbar/Navbar.tsx
+++ client/src/app/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { FaShoppingCart } from 'react-icons/fa'
 import customClient from '../../customClient'
 import { useEffect, useState } from 'react'
+import styles from './Navbar.module.css'
 
 interface INavBarData {
   logoLinkUrl: string
@@ -29,42 +30,57 @@ export default function Navbar() {
 
   if (navBarData.length > 0) {
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link
-              href={navBarData[0].logoLinkUrl}
-              aria-label={navBarData[0].logoLinkAriaLabel}
+      <nav className="bg-themeYellow-1">
+        <div className="flex justify-between py-6 px-4 md:px-12">
+          <ul className="flex justify-between gap-10 items-center uppercase font-semibold tracking-wide">
+            <li>
+              <Link
+                href={navBarData[0].logoLinkUrl}
+                aria-label={navBarData[0].logoLinkAriaLabel}
+              >
+                <div className="w-[150px]">
+                  <Image
+                    src={navBarData[0].logoLinkImage}
+                    alt=""
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: '100%', height: 'auto' }}
+                  />
+                </div>
+              </Link>
+            </li>
+            <li className="hidden md:flex">
+              <ul className="flex justify-between gap-10">
+                <li>
+                  <Link
+                    href="/products"
+                    className={styles.Navbar__desktop__link}
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/blog" className={styles.Navbar__desktop__link}>
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about" className={styles.Navbar__desktop__link}>
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <div className="text-3xl">
+            <button
+              aria-label="shopping cart"
+              onClick={() => alert('Products available for purchase soon!')}
             >
-              <div className="w-[150px]">
-                <Image
-                  src={navBarData[0].logoLinkImage}
-                  alt=""
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/products">Products</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-        <div>
-          <button
-            aria-label="shopping cart"
-            onClick={() => alert('Products available for purchase soon!')}
-          >
-            <FaShoppingCart />
-          </button>
+              <FaShoppingCart />
+            </button>
+          </div>
         </div>
       </nav>
     )
