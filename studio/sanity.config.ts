03a749d05a2f08b6008e9604b60a339f509d1076
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-07-15 11:24:28 -0400

    converted to monorepo

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -10,7 +10,7 @@ const singletonTypes = new Set(['about', 'blog', 'homepage', 'navbar', 'shop'])
 
 export default defineConfig({
   name: 'default',
-  title: 'cms',
+  title: 'studio',
 
   projectId: 'k8h63h1t',
   dataset: 'production',
